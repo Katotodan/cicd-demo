9998b1b59cd85c8b5a4212c6dd31ab0e8c40da5c
DANIEL KATOTO <katotodan@gmail.com>
2026-05-18 17:13:43 +0300

    Add app.js and initial tests

---
+++ app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('API Endpoints', () =>{
+    test('GET / returns message', async () =>{
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toBe('Hello CI/CD!');
+    })
+
+    test('GET /health returns healthy status', async () =>{
+        const response = await request(app).get('/health');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe('healthy');
+    })
+})
+
